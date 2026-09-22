96085bb1d6b7f31d04a5739fc0ea37ac46cc9d61
SPremji7 <168721989+SPremji7@users.noreply.github.com>
2026-09-21 20:35:31 -0400

    Add files via upload

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 output=$3
 tblastn -query $1 \
-  -subject $2 \
-  -task blastn-short \
+  -db $2 \
   -outfmt "6 std sseq qlen" -out $output
   
 awk '{if (($14*0.9>=$4) && ($3>=30)) {print $0}}' $output > hold.fna
